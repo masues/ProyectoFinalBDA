69268fefb511f91b5a3ba63a970e0afdff8fb730
Mario Suárez <masues64@gmail.com>
2021-02-05 00:06:26 -0600

    Modificación de script archivelog y adición de script para crear directorio faltante

--- s-08-config-archive-log.sql
+++ s-08-config-archive-log.sql
@@ -8,25 +8,30 @@ connect sys/system as sysdba
 
 -- Respaldar el spfile
 
-create pfile='/u01/app/oracle/oradata/ROSUPROY/disk_5/fast_recovery_area/rosubackup0.ora' from spfile;
+create pfile = 
+  '/u01/app/oracle/oradata/ROSUPROY/disk_5/fast_recovery_area/rosubackup0.ora'
+  from spfile;
 
---Empleado para configuramos 5 procesos ARCn requeridos
+--Empleado para configurar 5 procesos ARCn
 
 alter system set log_archive_max_processes= 5 scope=both;
 
 -- Ubicación de los archivos de control en el disco 1
 
-alter system set log_archive_dest_1='LOCATION=/u01/app/oracle/oradata/ROSUPROY/disk_1/archivelog/ MANDATORY' scope=both;
+alter system set log_archive_dest_1 = 
+  'LOCATION=/u01/app/oracle/oradata/ROSUPROY/disk_1/archivelog/ MANDATORY'
+  scope=both;
 
 -- Ubicación de los archivos de control en la fra
 
-alter system set log_archive_dest_1='LOCATION=/u01/app/oracle/oradata/ROSUPROY/disk_5/fast_recovery_area' scope=both;
+alter system set log_archive_dest_2='LOCATION=USE_DB_RECOVERY_FILE_DES'
+  scope=both;
 
 -- Formato para almacenar el nombre de los archive redologs
 
-alter system set  log_archive_format = arch_%t_%s_%r.arc scope=spfile;
+alter system set  log_archive_format='arch_rosuproy_%t_%s_%r.arc' scope=spfile;
 
--- Indica el número mínimo de copias que deben generarse con éxito son 1
+-- Indica el número mínimo de copias que deben generarse con éxito
 
 alter system set  log_archive_min_succeed_dest=1 scope=both;
 
@@ -38,4 +43,6 @@ alter database archivelog;
 
 alter database open;
 
-create spfile='/u01/app/oracle/oradata/ROSUPROY/disk_5/fast_recovery_area/rosubackup1.ora'from pfile;
+create pfile = 
+  '/u01/app/oracle/oradata/ROSUPROY/disk_5/fast_recovery_area/rosubackup1.ora'
+  from spfile;
---
+++ s-08-corrige-directorio.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# @Integrantes: 	Rosales Romero Ricardo
+#                 Suárez Espinoza Mario Alberto
+# @Fecha:         03 de febrero de 2021
+# @Descripción:   Script para crear el directorio faltante (archivelog)
+#                 EL SCRIPT DEBE EJECUTARSE CON EL USUARIO ROOT
+
+#Si no se usa a root, el script termina
+if ! [ ${USER} = "root" ]; then
+  echo "ERROR: El script debe ejecutarse con el usuario root"
+  exit 1
+fi
+
+mkdir -p /u01/app/oracle/oradata/ROSUPROY/disk_1/archivelog/
+chown oracle.oinstall /u01/app/oracle/oradata/ROSUPROY/disk_1/archivelog/
+chmod 755 /u01/app/oracle/oradata/ROSUPROY/disk_1/archivelog/
